0ff5799a71094dbff86886e3a4d89f255c7f8803
DerkT <derektan3510@gmail.com>
2022-12-23 19:59:31 -0800

    Created Java Maven project.

---
+++ src/main/java/com/drkwitht/ToyServer.java
@@ -0,0 +1,19 @@
+package com.drkwitht;
+
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.io.IOException;
+
+/**
+ * ToyServer.java
+ * This is the driver class containing the static HTTP server's main logic.
+ * @author Derek Tan
+ */
+public class ToyServer 
+{
+    
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
