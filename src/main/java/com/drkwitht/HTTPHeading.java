62812a8bbe400cd08f9b0777a3f28bcc5e85fc56
DerkT <derektan3510@gmail.com>
2022-12-27 09:53:31 -0800

    Added HTTP req heading data class.

---
+++ src/main/java/com/drkwitht/HTTPHeading.java
@@ -0,0 +1,29 @@
+package com.drkwitht;
+
+/**
+ * This data class encapsulates basic fields of an HTTP/1.0 or 1.1 <em>request</em> heading.
+ */
+public class HTTPHeading {
+    
+    private HTTPMethod requestMethod;
+    private String requestURL;
+    private String httpVersion;
+
+    public HTTPHeading (HTTPMethod method, String url, String version) {
+        requestMethod = method;
+        requestURL = url;
+        httpVersion = version;
+    }
+
+    public HTTPMethod fetchMethod() {
+        return requestMethod;
+    }
+
+    public String fetchURL() {
+        return requestURL;
+    }
+
+    public String fetchVersion() {
+        return httpVersion;
+    }
+}
