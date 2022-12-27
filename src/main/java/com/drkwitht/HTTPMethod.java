58d195dfafa16bb027b09792edaf509c42645301
DerkT <derektan3510@gmail.com>
2022-12-27 09:53:09 -0800

    Renamed HTTP method enum.

--- src/main/java/com/drkwitht/HTTPMethod.java
+++ src/main/java/com/drkwitht/HTTPMethod.java
@@ -3,7 +3,7 @@ package com.drkwitht;
 /**
  * Defines supported HTTP method codes. This will be used anywhere except server driver code.
  */
-public enum HTTPMethods {
+public enum HTTPMethod {
     HEAD,
     GET,
     POST,
