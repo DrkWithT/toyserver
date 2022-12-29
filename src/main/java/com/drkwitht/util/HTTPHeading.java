5b8b68e72e410776035fa7a1065acf50dc0f2f3b
DerkT <derektan3510@gmail.com>
2022-12-28 21:00:00 -0800

    Modified URL to class versus string.

--- src/main/java/com/drkwitht/util/HTTPHeading.java
+++ src/main/java/com/drkwitht/util/HTTPHeading.java
@@ -1,17 +1,19 @@
 package com.drkwitht.util;
 
+import java.net.MalformedURLException;
+import java.net.URL;
+
 /**
  * This data class encapsulates basic fields of an HTTP/1.0 or 1.1 <em>request</em> heading.
  */
 public class HTTPHeading {
-    
     private HTTPMethod requestMethod;
-    private String requestURL;
+    private URL requestURL;
     private String httpVersion;
 
-    public HTTPHeading (HTTPMethod method, String url, String version) {
+    public HTTPHeading (HTTPMethod method, String rawURL, String version) throws MalformedURLException {
         requestMethod = method;
-        requestURL = url;
+        requestURL = new URL(rawURL);
         httpVersion = version;
     }
 
@@ -19,7 +21,7 @@ public class HTTPHeading {
         return requestMethod;
     }
 
-    public String fetchURL() {
+    public URL fetchURL() {
         return requestURL;
     }
 
