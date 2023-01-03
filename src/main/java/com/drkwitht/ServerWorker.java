cb1aaa4a4f65702c604a25f33c2d6f878a2b2582
derektan <derektan@gmail.com>
2023-01-02 21:33:48 -0800

    Edited run logic as state machine.

--- src/main/java/com/drkwitht/ServerWorker.java
+++ src/main/java/com/drkwitht/ServerWorker.java
@@ -10,26 +10,34 @@ import java.time.ZonedDateTime;
 import java.time.format.DateTimeFormatter;
 import java.util.logging.Logger;
 
+import com.drkwitht.util.HTTPBody;
 import com.drkwitht.util.HTTPContentType;
 import com.drkwitht.util.HTTPHeader;
 import com.drkwitht.util.HTTPHeading;
 import com.drkwitht.util.HTTPMethod;
 import com.drkwitht.util.ServiceIssue;
+import com.drkwitht.util.ServiceState;
 
 /**
  * This is the runnable that is encapsulated in a worker thread for the server. A request is handled over a persistent server socket's connections. Request data is checked for basic HTTP syntax, valid methods and routes, and then for any <code>IOException</code>.
  */
 public class ServerWorker implements Runnable {
-    private Socket connection;
-    private BufferedReader requestStream;
+    private Socket connection;            // connection
+    private BufferedReader requestStream; 
     private PrintStream responseStream;
+    private DateTimeFormatter timeFormat; // GMT date generator
+    private Logger workerLogger;          // debug message printer
 
-    private DateTimeFormatter timeFormat;
-    private Logger workerLogger;
+    private SimpleRequest request;
+    private HTTPHeading reqHeading;
+    private HTTPBody reqBody;         // TODO: process later for more advanced requests
 
-    private ServiceIssue problemCode;
-    private String serverName;
-    private int reqBodyLength; // todo: use for req handling later
+    private ServiceState state;       // service state
+    private ServiceIssue problemCode; // client or server problem code
+    private int penaltyCount;         // deadly server exception count (2+ means stop!)
+    boolean hasKeepAlive;             // persistent connection flag
+    private String serverName;        // server software name
+    private int reqBodyLength;        // req body byte count
 
     public ServerWorker(String applicationName, Socket connectingSocket) throws IOException {
         connection = connectingSocket;
@@ -39,8 +47,15 @@ public class ServerWorker implements Runnable {
         timeFormat = DateTimeFormatter.RFC_1123_DATE_TIME;
         workerLogger = Logger.getLogger(this.getClass().getName());
 
+        request = null;
+        reqHeading = null;
+        reqBody = null;
+
+        state = ServiceState.START;
+        problemCode = ServiceIssue.NONE;
+        penaltyCount = 0;
+        hasKeepAlive = true;
         serverName = applicationName;
-        problemCode = ServiceIssue.BAD_REQUEST;
         reqBodyLength = 0;
     }
 
@@ -56,7 +71,38 @@ public class ServerWorker implements Runnable {
         return result;
     }
 
-    private SimpleResponse respondToIssues(ServiceIssue issueCode) {
+    private void handleHeaderState() throws IOException {
+        boolean hasHostHeader = false;
+        HTTPHeader aHeader = request.fetchHeader();
+        
+        while (aHeader != null) {
+            // stop header scanning at "Foo" (placeholder for empty HTTP line)
+            if (aHeader.fetchName() == "foo") {
+                break;
+            }
+
+            // require Host header
+            if (aHeader.fetchName().equals("Host")) {
+                hasHostHeader = true;
+            } else if (aHeader.fetchName().equals("Content-Length")) {
+                reqBodyLength = numberHTTPField(aHeader.fetchValueAt(0));
+            } else if (aHeader.fetchName().equals("Connection")) {
+                hasKeepAlive = aHeader.fetchValueAt(0).toLowerCase().equals("keep-alive");
+            }
+
+            aHeader = request.fetchHeader();
+        }
+
+        if (hasHostHeader) {
+            problemCode = ServiceIssue.NONE;
+            state = ServiceState.GET_BODY;
+        } else {
+            problemCode = ServiceIssue.BAD_REQUEST;
+            state = ServiceState.RESPOND;
+        }
+    }
+
+    private SimpleResponse respondAbnormal(ServiceIssue issueCode) {
         int statusNumber = 200;
         String statusMsg = "OK";
 
@@ -95,10 +141,10 @@ public class ServerWorker implements Runnable {
         boolean pathValid = routingPath.charAt(0) == '/' && !routingPath.contains("favicon.ico");
 
         if (!methodSupported)
-            return respondToIssues(ServiceIssue.NO_SUPPORT);
+            return respondAbnormal(ServiceIssue.NO_SUPPORT);
         
         if (!pathValid)
-            return respondToIssues(ServiceIssue.NOT_FOUND); // TODO: add favicon serving?
+            return respondAbnormal(ServiceIssue.NOT_FOUND); // TODO: add favicon serving?
         
         final String testHTML = "<html><head><title>A Page</title></head><body><p>Hello!</p></body></html>"; // TODO: refactor HTML into file fetcher class?
 
@@ -113,61 +159,71 @@ public class ServerWorker implements Runnable {
         return response;
     }
 
+    private void handleRespondState() throws IOException {
+        if (problemCode == ServiceIssue.NONE) {
+            responseStream.write(respondNormal(reqHeading.fetchMethod(), reqHeading.fetchURL()).asBytes());
+        } else {
+            responseStream.write(respondAbnormal(problemCode).asBytes());
+        }
+
+        if (!hasKeepAlive || penaltyCount > 1) {
+            state = ServiceState.STOP;
+        } else {
+            state = ServiceState.GET_REQUEST;
+        }
+    }
+
     @Override
     public void run() {
-        try {
-            SimpleRequest request = new SimpleRequest(requestStream);
-
-            HTTPHeading heading = request.fetchHeading();
-            
-            HTTPMethod method = heading.fetchMethod();
-            String relURL = heading.fetchURL();
-            //String httpName = heading.fetchVersion();
-
-            HTTPHeader aHeader = request.fetchHeader();
-
-            while (aHeader != null) {
-                // stop header scanning at "Foo" (placeholder for empty HTTP line)
-                if (aHeader.fetchName() == "foo") {
-                    break;
+        // advance through states for processing requests -> responses
+        while (state != ServiceState.STOP) {
+            try {
+                switch (state) {
+                    case START:
+                        state = ServiceState.GET_REQUEST;
+                        break;
+                    case GET_REQUEST:
+                        request = new SimpleRequest(requestStream);
+                        state = ServiceState.GET_HEADING;
+                        problemCode = ServiceIssue.NONE;
+                        break;
+                    case GET_HEADING:
+                        reqHeading = request.fetchHeading();
+                        state = ServiceState.GET_HEADERS;
+                        break;
+                    case GET_HEADERS:
+                        handleHeaderState();
+                        break;
+                    case GET_BODY:
+                        reqBody = request.fetchBody(HTTPContentType.UNKNOWN, reqBodyLength);
+                        break;
+                    case RESPOND:
+                        handleRespondState();
+                        break;
+                    case STOP:
+                    default:
+                        break;
                 }
-
-                // require Host header
-                if (aHeader.fetchName().equals("Host")) {
-                    problemCode = ServiceIssue.NONE;
-                } else if (aHeader.fetchName().equals("Content-Length")) {
-                    reqBodyLength = numberHTTPField(aHeader.fetchValueAt(0));
-                }
-
-                aHeader = request.fetchHeader();
+            } catch (IOException ioEx) {
+                workerLogger.warning(ioEx.toString());
+                problemCode = ServiceIssue.UNKNOWN;
+                state = ServiceState.RESPOND;
+                penaltyCount++;
+            } catch (Exception basicEx) {
+                workerLogger.warning(basicEx.toString());
+                problemCode = ServiceIssue.UNKNOWN;
+                state = ServiceState.RESPOND;
+                penaltyCount++; // track server err count for stopping infinite loop
             }
 
-            // skip body for now
-            request.fetchBody(HTTPContentType.UNKNOWN, reqBodyLength);
-
-            // handle good or bad requests here
-            SimpleResponse res;
-
-            if (problemCode == ServiceIssue.NONE) {
-                res = respondNormal(method, relURL);
-            } else {
-                res = respondToIssues(problemCode);
-            }
-
-            responseStream.write(res.asBytes());
-            responseStream.flush();
-
-        } catch (IOException ioError) {
-            workerLogger.warning(ioError.toString());
-        } catch (Exception genError) {
-            workerLogger.warning(genError.toString());
+            break;
         }
-        
-        // attempt to send final ACK by closing anyways...
+
+        // close connection when worker has finish state
         try {
             connection.close();
-        } catch (IOException ioError) {
-            workerLogger.warning(ioError.toString());
+        } catch (IOException ioEx) {
+            workerLogger.warning(ioEx.toString());
         }
     }
 }
